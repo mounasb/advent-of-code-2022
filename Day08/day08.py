67fc44a7852e72cc5fa392b7d811dea604fa9e29
mounasb <mounasb@pm.me>
2022-12-23 22:14:40 +0100

    day 08 - part 2

--- Day08/day08.py
+++ Day08/day08.py
@@ -3,27 +3,29 @@ import numpy as np
 with open("Day08/day08_input.txt") as f:
     content = f.read().splitlines()
 
-# Parsing
-# Generating a numpy array
+# Parsing : generating a numpy array
 grid = [list(x) for x in content]
 grid = [[int(x) for x in l] for l in grid]
 grid = np.array(grid)
 
 # Initializing constants
-sides = ((grid.shape[0] * 2) + (grid.shape[1] * 2)) - 4
+sides = ((grid.shape[0] * 2) + (grid.shape[1] * 2)) - 4     # nb of trees on the sides
 visible_trees = sides
 nb_rows = grid.shape[0]
 nb_columns = grid.shape[1]
 
+
+## PART ONE
+
 for i in range(1, nb_rows-1):
   for j in range(1, nb_columns-1):
     x = grid[i, j]    # isolating the tree
-    # inspecting left, right, top and bottom of the tree
-    z_row_left = grid[i,:j+1]
-    z_row_right = grid[i,j:nb_columns]
-    z_column_top = grid[:i+1,j]
-    z_column_bottom = grid[i:nb_rows,j]
-    sections = [z_row_left, z_row_right, z_column_top, z_column_bottom]
+    # inspecting left, right, top and bottom of the tree :
+    from_left = grid[i,:j+1]
+    from_right = grid[i,j:]
+    from_top = grid[:i+1,j]
+    from_bottom = grid[i:,j]
+    sections = [from_left, from_right, from_top, from_bottom]
 
     for section in sections:
       if np.count_nonzero(section == max(section)) == 1:    # only one max allowed
@@ -31,4 +33,32 @@ for i in range(1, nb_rows-1):
           visible_trees += 1
           break
 
-print(visible_trees)
+print("visible trees", visible_trees)
+
+
+## PART TWO
+
+view_scores = []
+
+for i in range(1, nb_rows-1):
+  for j in range(1, nb_columns-1):
+    x = grid[i, j]    # isolating the tree
+    # inspecting left, right, top and bottom of the tree :
+    look_left = grid[i,:j+1][-2::-1]
+    look_right = grid[i,j:][1:]
+    look_top = grid[:i+1,j][-2::-1]
+    look_bottom = grid[i:,j][1:]
+    inspections = [look_left, look_right, look_top, look_bottom]
+
+    view_score = 1
+    for inspection in inspections:
+      trees = 0
+      for k in inspection:
+        trees += 1
+        if k >= x:
+          break
+      view_score *= trees
+    
+    view_scores.append(view_score)
+
+print("best view_score", max(view_scores))
